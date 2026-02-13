ba3c811b464a5acaeb38e3bc84e3f54aa6de926a
Tauqeer2907 <aghariatauqeerabbas@gmail.com>
2026-02-13 14:11:07 +0530

    Updated API URL for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/expenses';
+const API_URL = 'https://tracker-backend.onrender.com/api/expenses';
 
 // Get all expenses
 export const getExpenses = async () => {
--- server/server.js
+++ server/server.js
@@ -20,4 +20,7 @@ app.use('/api/expenses', require('./routes/expenses'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
